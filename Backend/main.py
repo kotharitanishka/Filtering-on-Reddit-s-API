bc3a0a2fe3962fa861277d82e3014b8cb74e72bd
tanishka <tanishka.k.kothari@gmail.com>
2023-04-20 00:40:34 +0530

    first commit

---
+++ Backend/main.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template ,jsonify , request
+import urllib.request, json
+import os
+
+app = Flask(__name__)
+
+API_KEY = 'K1mBeF3EOiJzWQtjnScLBWMhGniJYBOZ'
+
+@app.route("/new")
+def get_images_new():
+    key = request.args.get('KEY')
+    if(key != API_KEY): return {'message' : "unauthorised" , 'success' : False}
+    
+    url = "https://www.reddit.com/r/images/new.json?limit=30"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    
+    children = dict['data']['children']
+    url_images = []
+    
+    for child in children:
+        url = child["data"]['url']
+        title = child["data"]['title']
+        if (child["data"]['is_video']) == False :
+            url_images.append({'url' : url , 'title' : title})
+
+    return {'message' : "authorised" , 'success' : True , 'data' : url_images}
+
+@app.route("/top")
+def get_images_top():
+    key = request.args.get('KEY')
+    if(key != API_KEY): return {'message' : "unauthorised" , 'success' : False}
+    
+    url = "https://www.reddit.com/r/images/top.json?limit=30&t=month"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    
+    children = dict['data']['children']
+    url_images = []
+    
+    for child in children:
+        url = child["data"]['url']
+        title = child["data"]['title']
+        if (child["data"]['is_video']) == False :
+            url_images.append({'url' : url , 'title' : title})
+
+    return {'message' : "authorised" , 'success' : True , 'data' : url_images}
+
+
+@app.route("/hot")
+def get_images_hot():
+    key = request.args.get('KEY')
+    if(key != API_KEY): return {'message' : "unauthorised" , 'success' : False}
+    
+    url = "https://www.reddit.com/r/images/hot.json?limit=30"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    
+    children = dict['data']['children']
+    url_images = []
+    
+    for child in children:
+        url = child["data"]['url']
+        title = child["data"]['title']
+        if (child["data"]['is_video']) == False :
+            url_images.append({'url' : url , 'title' : title})
+
+    return {'message' : "authorised" , 'success' : True , 'data' : url_images}
